b09e5180589c08fef1f380f5b502dd9c7e10c523
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2020-02-04 14:53:21 +0800

    Add files via upload

---
+++ day7/circlecalcapp/Server.java
@@ -0,0 +1,42 @@
+package day7.circlecalcapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Server
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			ServerSocket socketServer = new ServerSocket(9101);
+			System.out.println("Server started at " + new Date());
+			
+			Socket socket = socketServer.accept();
+			System.out.println("Client has connected.");
+			
+			DataInputStream fromClient = new DataInputStream(socket.getInputStream());
+			DataOutputStream toClient = new DataOutputStream(socket.getOutputStream());
+			
+			while(true)
+			{
+				double radius = fromClient.readDouble();
+				double area = Math.PI*radius*radius;
+				
+				toClient.writeDouble(area);
+				
+				System.out.println("Received radius = " + radius);
+				System.out.println("Sent area = " + area);
+				System.out.println();
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ day7/circlecalcmulticlientapp/Client.java
@@ -0,0 +1,42 @@
+package day7.circlecalcmulticlientapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			Socket socket = new Socket("192.168.123.100", 9101);
+			
+			DataInputStream fromServer = new DataInputStream(socket.getInputStream());
+			DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			while(true)
+			{
+				System.out.println("Please enter the radius value: ");
+				double radius = scanner.nextDouble();
+				
+				toServer.writeDouble(radius);
+				
+				double area = fromServer.readDouble();
+				
+				System.out.println("Sent radius = " + radius);
+				System.out.println("Received area = " + area);
+				System.out.println();
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+}
